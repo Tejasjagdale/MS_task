23461ad5bb4854012964b32162956be0cc8ba34a
tejas <tejasjagdale2003@gmail.com>
2022-11-04 09:09:35 +0530

    backend frontend intergration

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,11 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql");
 const multer = require("multer");
-const upload = multer();
+var fileupload = require("express-fileupload");
 
 const app = express();
+app.use(fileupload());
+app.use(express.static("public"));
 
 var corsOptions = {
   origin: "http://localhost:3000/",
@@ -14,14 +16,8 @@ var corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-//simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to site" });
-});
-
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -36,6 +32,24 @@ db.connect((err) => {
   }
   console.log("Connection done");
 });
+
+//simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to site" });
+});
+
+app.post("/upload", (req, res) => {
+  console.log(req.body)
+  let post = { filename: "file1", type: "pdf" };
+  let sql = "INSERT INTO fileupload SET ?";
+  let query = db.query(sql, post, (err, result) => {
+    if (err) throw err;
+    console.log("result");
+    res.send("Post 1 added");
+  });
+});
+
+
 
 // set port, listen for requests
 const PORT = process.env.PORT || 5000;
